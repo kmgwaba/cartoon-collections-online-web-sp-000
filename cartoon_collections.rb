997b6014b9c81690e05bca7d631f39512240efae
Kathia Mgwaba <mgwaba68@gmail.com>
2019-11-16 17:34:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ def summon_captain_planet(array)# code an argument here
    # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  array.any? do |word|
+    word.length > 4
+  end # Your code here
 end
 
 def find_the_cheese# code an argument here
