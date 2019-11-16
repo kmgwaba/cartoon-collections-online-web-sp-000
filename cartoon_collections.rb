edd0221f2922a50ed5755a852e18d75a61c4afc8
Kathia Mgwaba <mgwaba68@gmail.com>
2019-11-16 17:44:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,8 @@ def long_planeteer_calls(array)# code an argument here
 end
 
 def find_the_cheese(array)# code an argument here
-  array.include?("cheese_types")
-  return array  # the array below is here to help
+  array.find do |cheese|
+  cheese_types.include?(cheese)
+  end   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
